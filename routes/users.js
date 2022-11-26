ef5ef589623f98077049935fefeb222c4f41ecea
Kritsakorn <de.kritsakorn_st@tni.ac.th>
2022-11-26 11:21:29 +0700

    workshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(200).json({
+    fullname: 'Kritsakorn Dechalert',
+  });
+});
+
+router.get('/bio', function(req, res, next) {
+  res.status(200).json({
+    fullname: 'Kritsakorn Dechalert',
+    nickname: 'Kong',
+    hobby: 'Travel',
+    gitusername: 'KeroPunch'
+  });
 });
 
 module.exports = router;
