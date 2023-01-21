bc902d2cb3a47c0a8ad6389e4f4c3257396ad6a5
Kritsakorn <de.kritsakorn_st@tni.ac.th>
2023-01-21 10:20:34 +0700

    workshopA

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ router.get("/",shopController.index );
 router.get("/menu",shopController.menu );
 router.get("/:id",shopController.show );
 router.post("/",[
-    body('name').not().isEmpty().withMessage("กรุณาป้อนชื่อสกุลด้วย"),
+    body('name').not().isEmpty().withMessage("กรุณาป้อนชื่อบริษัทด้วย"),
     body('location.lat').not().isEmpty().withMessage("กรุณาป้อนพิกัดด้วย").isDecimal().withMessage("รูปแบบพิกัดไม่ถูกต้อง"),
     body('location.lgn').not().isEmpty().withMessage("กรุณาป้อนพิกัดด้วย").isDecimal().withMessage("รูปแบบพิกัดไม่ถูกต้อง")
 ],shopController.insert );
